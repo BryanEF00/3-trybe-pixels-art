f29650f2c64ed16a1842ad93db52e511f1ddab0d
Bryan Fernandes <bryan.efernandes@gmail.com>
2021-12-09 17:24:10 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-//grid
-let gridSize = 5;
-let grid = (gridSize * gridSize);
+const gridSize = 5;
+const grid = gridSize * gridSize;
 
 for (let i = 0; i < grid; i += 1) {
-  let pixel = document.createElement('div');
+  const pixel = document.createElement('div');
   pixel.className = 'pixel';
   document.getElementById('pixel-board').appendChild(pixel);
 }
-
-console.log(grid);
