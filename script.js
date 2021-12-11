5b803b49e582a2807d2ed3ea193210e48a79b2a0
Bryan Fernandes <bryan.efernandes@gmail.com>
2021-12-11 03:13:53 -0300

    melhorias requisito 1 ao 6, e requisito 7 feito

--- script.js
+++ script.js
@@ -1,13 +1,57 @@
-window.onload = function () {
-  const gridSize = 5;
-  const grid = gridSize * gridSize;
+/* Gerador de Cores */
 
-  for (let i = 0; i < grid; i += 1) {
-    const pixel = document.createElement('div');
-    pixel.className = 'pixel';
-    document.getElementById('pixel-board').appendChild(pixel);
+let numberColors = 4;
+
+for (let i = 0; i < numberColors; i += 1) {
+  let colorBox = document.createElement('div');
+  colorBox.classList.add('color');
+  document.querySelector('#color-palette').appendChild(colorBox);
+  if (i !== 0) {
+    /* Código referência: https://pt.stackoverflow.com/questions/493278/como-gerar-cores-hexadecimais-aleat%C3%B3rias-com-javascript
+    
+    Explicação:
+
+    - A função Math.floor arredonda o número para baixo, de modo a remover a parte decimal do número gerado pela função Math.random.
+    - O método Number.prototype.toString(16) converte o número em string em sua representação hexadecimal.
+    - O método String.prototype.padStart(6, '0') garante que a string hexadecimal tenha sempre seis dígitos. */
+
+    function randomColor() {
+      return (
+        '#' +
+        Math.floor(Math.random() * 0x1000000)
+          .toString(16)
+          .padStart(6, '0')
+      );
+    }
+    colorBox.style.backgroundColor = randomColor();
+  } else {
+    colorBox.style.backgroundColor = 'black';
   }
+}
 
-  let initialColor = document.getElementById('black');
-  initialColor.classList.add('selected');
+/* Gerador de Grid */
+
+let boardSize = 5;
+let boardGrid = boardSize * boardSize;
+
+for (let i = 0; i < boardGrid; i += 1) {
+  let pixelUnit = document.createElement('div');
+  pixelUnit.classList.add('pixel');
+  document.querySelector('#pixel-board').appendChild(pixelUnit);
+}
+
+/* Seletor */
+let colorPalette = document.querySelector('#color-palette').children;
+
+window.onload = function () {
+  colorPalette[0].classList.add('selected');
 };
+
+for (let i = 0; i < numberColors; i += 1) {
+  colorPalette[i].addEventListener('click', function () {
+    for (const key of colorPalette) {
+      key.classList.remove('selected');
+    }
+    this.classList.add('selected');
+  });
+}
