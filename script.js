79f7f167c0aca9886f7004ab6bc408a68c1b6c04
Bryan Fernandes <bryan.efernandes@gmail.com>
2021-12-17 03:33:32 -0300

    Requisitos Bônus + Reestruturação geral do código

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 /* Gerador de Cores */
-
 let numberColors = 4;
 
 for (let i = 0; i < numberColors; i += 1) {
@@ -7,14 +6,6 @@ for (let i = 0; i < numberColors; i += 1) {
   colorBox.classList.add('color');
   document.querySelector('#color-palette').appendChild(colorBox);
   if (i !== 0) {
-    /* Código referência: https://pt.stackoverflow.com/questions/493278/como-gerar-cores-hexadecimais-aleat%C3%B3rias-com-javascript
-    
-    Explicação:
-
-    - A função Math.floor arredonda o número para baixo, de modo a remover a parte decimal do número gerado pela função Math.random.
-    - O método Number.prototype.toString(16) converte o número em string em sua representação hexadecimal.
-    - O método String.prototype.padStart(6, '0') garante que a string hexadecimal tenha sempre seis dígitos. */
-
     function randomColor() {
       return (
         '#' +
@@ -24,57 +15,89 @@ for (let i = 0; i < numberColors; i += 1) {
       );
     }
     colorBox.style.backgroundColor = randomColor();
+
+    /* Código referência: https://pt.stackoverflow.com/questions/493278/como-gerar-cores-hexadecimais-aleat%C3%B3rias-com-javascript
+    
+    Explicação:
+    - A função Math.floor arredonda o número para baixo, de modo a remover a parte decimal do número gerado pela função Math.random.
+    - O método Number.prototype.toString(16) converte o número em string em sua representação hexadecimal.
+    - O método String.prototype.padStart(6, '0') garante que a string hexadecimal tenha sempre seis dígitos. */
   } else {
     colorBox.style.backgroundColor = 'black';
   }
 }
 
-/* Gerador de Grid */
-
-let boardSize = 5;
-let boardGrid = boardSize * boardSize;
-
-for (let i = 0; i < boardGrid; i += 1) {
-  let pixelUnit = document.createElement('div');
-  pixelUnit.classList.add('pixel');
-  document.querySelector('#pixel-board').appendChild(pixelUnit);
-}
-
-/* Seletor */
-let colorPalette = document.querySelector('#color-palette').children;
-
+/* Cor Preta Selecinada Inicialmente */
 window.onload = function () {
-  colorPalette[0].classList.add('selected');
+  let initialColor = document.getElementById('color-palette').children[0];
+  initialColor.classList.add('selected');
 };
 
+/* Seletor de Cor */
+let colorBoard = document.querySelector('#color-palette').children;
 for (let i = 0; i < numberColors; i += 1) {
-  colorPalette[i].addEventListener('click', function () {
-    for (const key of colorPalette) {
-      key.classList.remove('selected');
-      key.classList.remove('selectedIndicator');
+  colorBoard[i].addEventListener('click', function () {
+    for (let i = 0; i < numberColors; i += 1) {
+      colorBoard[i].classList.remove('selected');
+      colorBoard[i].classList.remove('highLight');
     }
-    this.classList.add('selected');
-    this.classList.add('selectedIndicator');
+    colorBoard[i].classList.add('selected');
+    colorBoard[i].classList.add('highLight');
   });
 }
-
-/* Cor no grid */
-
-let pixel = document.querySelector('#pixel-board').children;
-
-for (let i = 0; i < boardGrid; i += 1) {
-  pixel[i].addEventListener('click', function () {
-    this.style.backgroundColor =
-      document.querySelector('.selected').style.backgroundColor;
+/* Grid Inicial */
+for (let i = 0; i < 25; i += 1) {
+  let pixel = document.createElement('div');
+  pixel.classList.add('pixel');
+  document.getElementById('pixel-board').appendChild(pixel);
+  pixel.addEventListener('click', function () {
+    pixel.style.backgroundColor =
+      document.getElementsByClassName('selected')[0].style.backgroundColor;
   });
 }
+/* Botão Clear Board */
+let clearBtn = document.getElementById('clear-board');
 
-/* Limpa o grid */
+clearBtn.addEventListener('click', function () {
+  let clearArea = document.getElementsByClassName('pixel').length;
+  for (i = 0; i < clearArea; i += 1) {
+    document.getElementsByClassName('pixel')[i].style.backgroundColor = 'white';
+  }
+});
 
-let clearGrid = document.querySelector('#clear-board');
+/* Botão do Input */
+let alertBtn = document.getElementById('generate-board');
+let input = document.getElementById('board-size');
 
-clearGrid.addEventListener('click', function () {
-  for (let i = 0; i < boardGrid; i += 1) {
-    pixel[i].style.backgroundColor = 'white';
+alertBtn.addEventListener('click', function () {
+  /* Verificação */
+  if (input.value === '') {
+    alert('Board inválido!');
+  } else if (input.value < 5) {
+    input.value = 5;
+  } else if (input.value > 50) {
+    input.value = 50;
+  }
+
+  /* Remove Grid */
+  let removeAreaSize = document.querySelector('#pixel-board').children.length;
+
+  for (let i = 0; i < removeAreaSize; i += 1) {
+    let removePixel = document.querySelector('#pixel-board').children;
+    removePixel[0].remove();
+  }
+
+  /* Adiciona Novo Grid */
+  document.getElementById('pixel-board').style.gridTemplateColumns =
+    'repeat(' + input.value + ', auto)';
+
+  for (let i = 0; i < input.valueAsNumber ** 2; i += 1) {
+    let pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    document.getElementById('pixel-board').appendChild(pixel);
+    pixel.addEventListener('click', function () {
+      pixel.style.backgroundColor =
+        document.getElementsByClassName('selected')[0].style.backgroundColor;
+    });
   }
 });
